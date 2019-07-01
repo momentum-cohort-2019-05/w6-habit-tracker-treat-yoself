901cc009d9f9e3580815ff07d5605821b7e5d4d5
Kyle Heidelberger <kyle.heidelberger@gmail.com>
2019-07-01 17:03:33 -0400

    update models for habit and dailyrecord

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import date
 
 # Create your models here.
 
@@ -11,20 +12,21 @@ class Habit(models.Model):
                                            help_text="How many units is your goal?")
     unit = models.CharField(
         max_length=200, help_text="What units are you completing? (steps, lines of code, hours, etc.")
-    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    date_started = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.activity} {self.goal_num} {self.unit}"
 
 
 class DailyRecord(models.Model):
-    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
-    user_habit = models.ForeignKey(to=Habit, on_delete=models.CASCADE)
-    date = models.DateField(auto_now_add=True)
+    num_achieved = models.PositiveIntegerField(default=0)
+    owner_habit = models.ForeignKey(to=Habit, on_delete=models.CASCADE)
+    date = models.DateField(default=date.today)
 
 # need to change this string so it shows date and the input of the user that day
     def __str__(self):
-        return f"{self.date} | {self.user_habit}"
+        return f"{self.date} | {self.owner_habit}"
 
     class Meta:
-        unique_together = ['user', 'user_habit']
+        unique_together = ['date', 'owner_habit']

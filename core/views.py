48dd8e3ee0666f49650743be5c081be04794f5b6
Kyle Heidelberger <kyle.heidelberger@gmail.com>
2019-07-01 13:51:18 -0400

    add index page

--- core/views.py
+++ core/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from .models import Habit, DailyRecord
 # Create your views here.
+
+
+def index(request):
+    """
+    View function for home page of site.
+    """
+    habit_list = Habit.objects.all()
+
+    
+
+
+    context = {
+        'habit_list': habit_list,
+    }
+
+    return render(request, 'index.html', context=context)
--- habittracker/urls.py
+++ habittracker/urls.py
@@ -16,11 +16,20 @@ Including another URLconf
 from django.conf import settings
 from django.urls import path, include
 from django.contrib import admin
+from django.views.generic import RedirectView
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('registration.backends.simple.urls')),
+    path('core/', include('core.urls')),
+    path('', RedirectView.as_view(url='/core/', permanent=True)),
+
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
 if settings.DEBUG:
     import debug_toolbar
